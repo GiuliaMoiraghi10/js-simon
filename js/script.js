7fd99bc7052e83900f648fda45d5feb25f3d2b69
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-24 14:45:07 +0200

    Scansione problema js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('Ciao')
+
+// Creare Countdown da 30 sec
+
+// Generare 5 numeri random
+
+// Al termine del countdown
+//  - Numeri scompaiono
+//  - Compaiono form input
+
+// Utente invia numeri tramite un bottone
+
+// SE numeri corrispondono a numeri random
+//  - vittoria
+// ALTRIMENTI
+//  -perdita
